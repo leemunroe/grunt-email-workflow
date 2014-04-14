41d11446f4c6f718044dde10faad072fd0f9e0ab
Lee Munroe <lee@leemunroe.com>
2014-04-14 16:40:54 -0700

    Add test emails with Mailgun

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,6 +36,18 @@ module.exports = function(grunt) {
               'build/transaction.html': ['build/*.html']
             }
           }
+        },
+
+        mailgun: {
+          mailer: {
+            options: {
+              key: 'key-3ax6xnjp29jd6fds4gc373sgvjxteol0',
+              sender: 'devs@mailgun.net',
+              recipient: 'lee@mailgun.com',
+              subject: 'This is a test email'
+            },
+            src: ['build/*.html']
+          }
         }
 
     });
@@ -44,8 +56,9 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('assemble');
     grunt.loadNpmTasks('grunt-premailer');
+    grunt.loadNpmTasks('grunt-mailgun');
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['sass','assemble','premailer']);
+    grunt.registerTask('default', ['sass','assemble','premailer','mailgun']);
 
 };
