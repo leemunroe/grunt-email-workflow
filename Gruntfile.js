e99166b283a81606a97ef8510c51fb12ef447e0b
Lee Munroe <lee@leemunroe.com>
2015-07-08 10:48:08 -0700

    Fix cdnify image dir source

--- Gruntfile.js
+++ Gruntfile.js
@@ -170,7 +170,7 @@ module.exports = function(grunt) {
             'region': '<%= secrets.cloudfiles.region %>', // See README for secrets.json or replace this with your region
             'upload': [{
               'container': '<%= secrets.cloudfiles.container %>', // See README for secrets.json or replace this with your container name
-              'src': '<%= paths.src_img %>/*',
+              'src': '<%= paths.dist_img %>/*',
               'dest': '/',
               'stripcomponents': 0
             }]
