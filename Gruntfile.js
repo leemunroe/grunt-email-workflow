440308050fe578d40a29f9c81bffa5327a48e7d4
Matt Bain <mattb@solegraphics.com>
2015-07-10 19:41:16 -0700

    Remove duplicate, grunt serve command

--- Gruntfile.js
+++ Gruntfile.js
@@ -341,11 +341,6 @@ module.exports = function(grunt) {
 
     // Launch the express server and start watching
     // NOTE: The server will not stay running if the grunt watch task is not active
-    grunt.registerTask('serve', ['default', 'express', 'open', 'watch']);
-
-
-    // Launch the express server and start watching
-    // NOTE: The server will not stay running if the grunt watch task is not active
     grunt.registerTask('serve', ['default', 'autoprefixer:preview', 'express', 'open', 'watch']);
 
 };
